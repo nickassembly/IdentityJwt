16a99fe85da9aa8682f5083b269fd9b5131ec868
Nick Guerra <nickg@maliciousgames.com>
2020-04-06 18:53:52 -0500

    added pipeline for identity

---
+++ IdentityJWT.API/Models/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityJWT.API.Models
+{
+   public class ApplicationDbContext : IdentityDbContext
+   {
+
+      public ApplicationDbContext(DbContextOptions options) : base(options)
+      {
+
+      }
+
+      public DbSet<Employee> Employees { get; set; }
+
+    }
+}
